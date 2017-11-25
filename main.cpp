80a05f2a3f2ba5bf195a55d06dc7a4ad4f772bf5
Henrique de Souza La Porta <henriqueslaporta@gmail.com>
2017-11-25 11:29:29 -0200

    [Add] metodo guloso funcionando

--- main.cpp
+++ main.cpp
@@ -12,142 +12,23 @@ using namespace std;
 ifstream fileHandle;
 char buffer[256];
 int status;
+
 /* Constantes */
 int numVertices; //numero de vértices do grafo
 int costLimit;  //Numero limite de distancia possivel
 int verticeInit; //Vertice inicial]
 
 float **indexVertice;
-int *custoVertice;
+int *pontosVertice;
 int **adjacentMat;
 int *solucaoAtual;
 
+int pontosAtual;
+int custoAtual;
 
-int readFirstLines(){
-    char *token;
-    if(fileHandle.eof()){ return ERROR;}
+#include "arquivo.cpp"
+#include "guloso.cpp"
 
-    for(int i = 0; i < 3; i++){
-        fileHandle.getline (buffer,100);
-    }
-
-    // Le o numero de vertices
-    fileHandle.getline(buffer,100);
-    token = std::strtok(buffer, " ");
-    token = std::strtok(NULL, " ");
-    token = std::strtok(NULL, " ");
-    numVertices = atoi(token);
-
-    // Le o limite de distancia
-    fileHandle.getline(buffer,100);
-    token = std::strtok(buffer, " ");
-    token = std::strtok(NULL, " ");
-    token = std::strtok(NULL, " ");
-    costLimit = atoi(token);
-
-    cout << numVertices << "\n";
-    cout << costLimit << "\n";
-
-    return 0;
-}
-
-int readCoordSection(){
-    char *token;
-    if(fileHandle.eof()){ return ERROR;}
-
-    indexVertice = new float *[numVertices];
-    for(int n = 0; n < numVertices; n++)
-          indexVertice[n] = new float[2];
-
-    //Pula duas linhas
-    fileHandle.getline (buffer,100);
-    fileHandle.getline (buffer,100);
-
-    for(int i = 0; i < numVertices; i++){
-        fileHandle.getline (buffer,100);
-        token = std::strtok(buffer, " ");
-        int index = atoi(token);
-        token = std::strtok(NULL, " ");
-        indexVertice[index-1][0] = atol(token);
-        token = std::strtok(NULL, " ");
-        indexVertice[index-1][1] = atol(token);
-    }
-
-     //Print matriz
-    for(int i = 0; i < numVertices;i++)
-        cout << "index[" << i << "] (" <<indexVertice[i][0] << ") (" << indexVertice[i][1] << ")\n";
-    /* */
-    return 0;
-}
-
-int readScoreSection(){
-    char *token;
-    if(fileHandle.eof()){ return ERROR;}
-
-    custoVertice = new int [numVertices];
-
-    //Pula uma linha
-    fileHandle.getline (buffer,100);
-
-    for(int i = 0; i < numVertices; i++){
-        fileHandle.getline (buffer,100);
-        token = std::strtok(buffer, " ");
-        int index = atoi(token);
-        token = std::strtok(NULL, " ");
-        custoVertice[index-1] = atol(token);
-    }
-
-     //Print matriz
-    for(int i = 0; i < numVertices;i++)
-        cout << "index[" << i << "] (" << custoVertice[i] << ")\n";
-    /* */
-    return 0;
-}
-
-int readInitVertice(){
-    char *token;
-    if(fileHandle.eof()){ return ERROR;}
-
-    //Pula uma linha
-    fileHandle.getline (buffer,100);
-
-    fileHandle.getline (buffer,100);
-    verticeInit = atoi(buffer);
-
-    /* //Print
-    cout << "Vertice inicial[" << verticeInit << "]\n";
-    /* */
-    return 0;
-}
-
-int readFile(){
-    status = readFirstLines();
-    if(status==ERROR){return ERROR;}
-
-    status = readCoordSection();
-    if(status==ERROR){return ERROR;}
-
-    status = readScoreSection();
-    if(status==ERROR){return ERROR;}
-
-    status = readInitVertice();
-    if(status==ERROR){return ERROR;}
-
-    fileHandle.close();
-    return 0;
-}
-
-int openFile(char* file){
-    fileHandle.open(file);
-    if (! fileHandle.is_open()){
-            cout << "Error opening file";
-            return ERROR;
-    }
-    else{
-        cout << "Opening file: " << file << "\n";
-        return 0;
-    }
-}
 
 int calcAdjacentMat(){
     int xd, yd;
@@ -164,7 +45,12 @@ int calcAdjacentMat(){
         }
     }
 
-    //Print matriz
+    /*//Print matriz
+    cout << "Vertice";
+    for(int i = 0; i < numVertices; i++){
+        cout << "    " << i; 
+    }
+    cout << "\n";
     for(int i = 0; i < numVertices; i++){
         cout << "index[" << i << "]";
         for(int j = 0; j < numVertices; j++){
@@ -172,25 +58,38 @@ int calcAdjacentMat(){
         }
         cout << "\n";
     }
+    cout << "\n";
     /* */
 
     return 0;
 }
 
-int greedy(){
-    int *solucaoAux = new int [numVertices];
+void printInfos(){
+	cout << "Infos:\n";
+	cout << "Numero de Vertices: " << numVertices << "\tCusto Limite: " << costLimit << "\n";
+	cout << "Solucao Atual:\nCusto: " << custoAtual << "\tPremio: " << pontosAtual << "\n";
+	cout << solucaoAtual[0];
+    for(int i = 1; i < numVertices+1;i++){
+        if(solucaoAtual[i] != -1)
+        	cout << " -> " << solucaoAtual[i];
+    }
+    cout << "\n";
 }
 
 int main(){
-    status = openFile("instances/a8.oplib");
+    status = openFile("instances/bier127.oplib");
     if(status==ERROR){return ERROR;}
 
     status = readFile();
     if(status==ERROR){return ERROR;}
 
-    solucaoAtual = new int [numVertices];
+    solucaoAtual = new int [numVertices + 1];
 
     calcAdjacentMat();
+
+	greedy();
+	
+	printInfos();
 
     return 0;
 }
